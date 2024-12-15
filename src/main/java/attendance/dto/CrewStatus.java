c256e31d56030a2424bae86cf7b49324b3ebbeee
Chaeruin <cms3604@naver.com>
2024-12-15 12:28:09 +0900

    feat: validator 구현 완ㄹ

--- src/main/java/attendance/dto/CrewStatus.java
+++ src/main/java/attendance/dto/CrewStatus.java
@@ -1,4 +1,6 @@
 package attendance.dto;
 
 public record CrewStatus() {
+
+
 }
--- src/main/java/attendance/utils/InputValidator.java
+++ src/main/java/attendance/utils/InputValidator.java
@@ -1,5 +1,29 @@
 package attendance.utils;
 
+import attendance.enums.ErrorMessage;
+
 public class InputValidator {
+    // 메뉴 입력 문자가 아닌 경우
+    public static boolean isMenuInput(String input) {
+        if (!(input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("Q"))) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
+        return true;
+    }
+
+    // 유효하지 않은 시간 입력 예외
+    public static boolean isValidHour(String input) {
+        String[] hourMinute = input.split(":");
+        int hour = Integer.parseInt(hourMinute[0]);
+        int minute = Integer.parseInt(hourMinute[1]);
+        if (hour > 24 || hour < 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
+        if (minute > 59 || minute < 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
+        return true;
+    }
+
     
 }
